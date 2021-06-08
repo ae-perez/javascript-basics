841f49e958414240102605dc67265c1922dfaf60
Alejandra <perezalejandra.e@gmail.com>
2021-06-08 12:25:17 -0400

    finished exercised for part 3

--- fundamentals-part2.js
+++ fundamentals-part2.js
@@ -11,13 +11,25 @@ function multiply () {
 }
 
 //problem three ********************
-var str = "hello";
-
+var str = "HELLO";
 function capitalize(str) {
-    str = str.split('');
+    str = str.toLowerCase();
+    str = str[0].toUpperCase() + str.substring(1);
+    //remember strings have some features of arrays which is why the str[0] works above
+    // also substring() .. why it was used --> it is a method that retusn the part of the string between the start/end of indexes
+        // so we told the computer return the rest of the string starting from string 1
+
     return str;
 }
-
-capitalize();
+console.log(capitalize(str));
 
 //problem four ********************
+var str = "abcd";
+
+function lastLetter(str) {
+    let len = str.length;
+
+    return str[len-1];
+
+}
+console.log(lastLetter(str));
