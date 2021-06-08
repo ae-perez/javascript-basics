da53d365a39f465be4b8bf3fef20f6d7afaaf083
Alejandra <perezalejandra.e@gmail.com>
2021-06-08 01:14:07 -0400

    add changes to fundamentals part 2, still incomplete

--- fundamentals-part2.js
+++ fundamentals-part2.js
@@ -1,1 +1,23 @@
-console.log("testing");
+//assoiciated with fundamentals part 3: section --> functions
+
+//problem one ********************
+function add7 (num) {
+    return num + 7;
+}
+
+//problem two ********************
+function multiply () {
+    return num1 * num2;
+}
+
+//problem three ********************
+var str = "hello";
+
+function capitalize(str) {
+    str = str.split('');
+    return str;
+}
+
+capitalize();
+
+//problem four ********************
