a4fe7d5225280e8ce7b4c2d0670d2ea7d72195fd
Alejandra <perezalejandra.e@gmail.com>
2021-06-11 17:04:10 -0400

    fizzbuzz function creations

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+/*
+Write a program that takes a user’s input and prints the numbers from one to the number the user entered. 
+However, for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. 
+For numbers which are multiples of both three and five print FizzBuzz.
+*/
+
+function fizzbuzz(input) {
+    let arr = [0];
+    for(let i = 1; i < input; i++) {
+
+    }
+
+
+    return arr; //change this
+}
+
+var input = 22;
+console.log(fizzbuzz(input));
+
+/*
+Notes:
+- starts at 1 
+- ends at n
+- multiples of 3 = fizz
+- multiple of 5 = buzz
+*/
