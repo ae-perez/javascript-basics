fde63b23c44c30b291b68b00f112afa8e2fb1efc
Alejandra <perezalejandra.e@gmail.com>
2021-06-15 15:06:45 -0400

    finishing rock paper scissors game

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,20 +1,45 @@
 function computerPlay() {
-    let ret = [1, 2, 3]; //0 , 1, 2
-    let n = 0;
-    
-    function pickNum(ret) {
-        n = Math.floor(Math.random() * ret);
-        console.log("testing1");
-        return n;
-    }
+    let rps = ['rock', 'paper', 'scissors'];
+    let ret = (rps[Math.floor(Math.random() * rps.length)]);
 
-    pickNum(ret);
+    console.log(ret); //delete this @ the very end
 
-    return console.log(n); //NaN
+    return ret;
 }
-
-computerPlay();
 
 function playRound(pSelection, cSelection) {
+    pSelection = pSelection.toLowerCase();
+    let ret = '0';
+
+    if (pSelection == 'rock' && cSelection == 'paper') {
+        ret = 'You Lose! Paper beats Rock.'
+    }
+    else if (pSelection == 'rock' && cSelection == 'scissors') {
+        ret = 'You Win! Rock beats Scissors.'
+    }
+    else if (pSelection == 'paper' && cSelection == 'rock') {
+        ret = 'You Win! Paper beats Rock.'
+    }
+    else if (pSelection == 'paper' && cSelection == 'scissors') {
+        ret = 'You Lose! Scissors beats Paper.'
+    }
+    else if (pSelection == 'scissors' && cSelection == 'rock') {
+        ret = 'You Lose! Rock beats Scissors.'
+    }
+    else if (pSelection == 'scissors' && cSelection == 'paper') {
+        ret = 'You Lose! Scissors beats Paper.'
+    }
+    else  { //code when things are tied
+        ret = 'Draw!'
+    }
+    
+    return ret;
+}
+
+function game() {
 
 }
+
+const pSelection = "rock";
+const cSelection = computerPlay();
+console.log(playRound(pSelection, cSelection));
